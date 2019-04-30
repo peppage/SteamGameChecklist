c54cbe1369ba7d7c45bfb10cfece3b43511ac08d
Michael Peppler <mpeppler@carnegiehall.org>
2019-04-30 12:35:19 -0400

    fix svelte build directory

--- src/SteamGameChecklist.Web/Startup.cs
+++ src/SteamGameChecklist.Web/Startup.cs
@@ -26,7 +26,7 @@ namespace SteamGameChecklist.Web
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/build";
+                configuration.RootPath = "ClientApp/public";
             });
 
             services.AddSingleton(Configuration);
